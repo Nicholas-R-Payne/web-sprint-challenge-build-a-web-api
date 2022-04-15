cec25e9b2952e8d45ba3736017975c051e6f27e4
DarkedgeTH <darkedgeth@gmail.com>
2022-04-14 20:39:24 -0400

    GET request for /api/projects working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const Project = require('./projects-model.js')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Project.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+})
+
+module.exports = router;
