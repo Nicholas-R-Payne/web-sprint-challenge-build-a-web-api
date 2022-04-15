0e195f8f1b809f5c8642516c4177a1f1e0daee36
DarkedgeTH <darkedgeth@gmail.com>
2022-04-14 20:51:25 -0400

    GET request for /api/projects/:id working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,4 +13,23 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await Project.get(req.params.id)
+        if (!project) {
+            res.status(404).json({
+                message: "The project with the specified ID does not exist"
+            })
+        } else {
+            res.json(project)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The project information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    }
+})
+
 module.exports = router;
