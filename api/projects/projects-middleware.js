581928a14395f32c2158b1e9321c1f47b42e7e3f
DarkedgeTH <darkedgeth@gmail.com>
2022-04-15 09:44:05 -0400

    created and initialized validateProjectBody middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -21,6 +21,26 @@ async function validateProjectId(req, res, next) {
     }
 }
 
+async function validateProjectBody(req, res, next) {
+    try {
+        const { name, description, completed } = req.body
+        if (!name || !description || !completed == null) {
+            res.status(400).json({
+                message: 'Required field is missing'
+            })
+        } else {
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The project information could not be retrieved",
+            err: err.message,
+            stack: err.stack
+        })
+    }
+}
+
 module.exports = {
-    validateProjectId
+    validateProjectId,
+    validateProjectBody
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,49 +1,30 @@
 // Write your "projects" router here!
 const express = require('express')
 
-const { validateProjectId } = require('./projects-middleware')
+const { validateProjectId, validateProjectBody } = require('./projects-middleware')
 
 const Project = require('./projects-model')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     Project.get()
         .then(projects => {
             res.json(projects)
         })
-        .catch(err => {
-            res.status(500).json({
-                message: "The projects information could not be retrieved",
-                err: err.message,
-                stack: err.stack
-            })
-        })
+        .catch(next)
 })
 
 router.get('/:id', validateProjectId, (req, res) => {
     res.json(req.project)
 })
 
-router.post('/', (req, res) => {
-    const { name, description } = req.body
-    if (!name || !description) {
-        res.status(400).json({
-            message: "Please provide name and description for the project"
+router.post('/', validateProjectBody, (req, res, next) => {
+    Project.insert(req.body)
+        .then(projects => {
+            res.status(201).json(projects)
         })
-    } else {
-        Project.insert(req.body)
-            .then(projects => {
-                res.status(201).json(projects)
-            })
-            .catch(err => {
-                res.status(500).json({
-                    message: "There was an error while saving the project to the database",
-                    err: err.message,
-                    stack: err.stack
-                })
-            })
-    }
+        .catch(next)
 })
 
 module.exports = router;
